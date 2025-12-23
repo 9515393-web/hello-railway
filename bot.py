c8c78bf8831ddda0181ea3b5e0496a70bc11030a
Artem <9515393@gmail.com>
2025-12-23 15:40:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,47 @@
 import os
+import sqlite3
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+DB_NAME = "votes.db"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            tg_id INTEGER UNIQUE
+        )
+    """)
+    conn.commit()
+    conn.close()
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот запущен 🚀")
+    tg_id = update.effective_user.id
+
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    cur.execute("INSERT OR IGNORE INTO users (tg_id) VALUES (?)", (tg_id,))
+    conn.commit()
+    conn.close()
+
+    await update.message.reply_text(
+        "Привет! 👋\n"
+        "Ты записан в базу данных.\n"
+        "Бот работает с SQLite ✅"
+    )
+
 
 def main():
+    init_db()
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
